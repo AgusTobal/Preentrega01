bf27d8000844f47a1afaa3762ef23a861f2ef9ef
AgusTobal <tobalagustin2@gmail.com>
2023-05-19 16:17:44 -0300

    Preentrega

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Navbar from './Navbar'
+import ItemListContainer from './ItemListContainer'
+import Cardwidget from './Cardwidget'
+
+ 
+  const App = () => {
+
+  return (
+    <>
+    
+    <Navbar/>
+    <ItemListContainer />
+    <cardwidget/>
+    
+    
+
+    
+    
+
+    </>
+  )
+}
+
+export default App
